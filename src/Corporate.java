ecff979a233b4b1453ebd275fa3b1d42950bd8ef
Th0github <tdtsijpkens@gmail.com>
2020-05-12 11:04:42 +0200

    Modified NormalNumberGenerator.java to include a generate() method which just returns the final output of z.

--- src/Corporate.java
+++ src/Corporate.java
@@ -3,6 +3,10 @@ public class Corporate extends PoissonProcess implements CustomerServiceAgent {
     private final double RATE_EIGHT_am_to_SIX_pm = 1.0;
     private final double RATE_SIX_am_to_EIGHT_am = 0.2;
 
+    //Time per hour, minute, etc.
+    private final double MINUTE_TO_HOUR = 60;
+    private final double SECOND_TO_HOUR = 3600; //this is what we will be using
+
     @Override
     public boolean handle() {
         return true;
--- src/NormalNumberGenerator.java
+++ src/NormalNumberGenerator.java
@@ -2,25 +2,33 @@
  * @author ohnoyes
  */
 public class NormalNumberGenerator {
+    public double r, x, y, z;
     /**
      * Uses the Box and Muller method for generating Normal random numbers.
      */
     public NormalNumberGenerator() {
-        double r, x, y;
 
         // find a uniform random point (x, y) inside unit circle
         do {
             x = 2.0 * Math.random() - 1.0;
             y = 2.0 * Math.random() - 1.0;
             r = x*x + y*y;
-        } while (r > 1 || r == 0);    // loop executed 4 / pi = 1.273.. times on average
+        } while (r > 1 || r == 0);
         // http://en.wikipedia.org/wiki/Box-Muller_transform
 
 
-        // apply the Box-Muller formula to get standard Gaussian z
-        double z = x * Math.sqrt(-2.0 * Math.log(r) / r);
+        // formula for Normal Distribution z
+        z = x * Math.sqrt(-2.0 * Math.log(r) / r);
 
-        // print it to standard output
+        // print it
         System.out.println(z);
+    }
+
+    /**
+     * generates the random variate on the normal distribution
+     * @return the generated random variate
+     */
+    public double generate(){
+        return this.z;
     }
 }
