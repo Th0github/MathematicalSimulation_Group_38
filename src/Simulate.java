ed13f449d3ec1091f1bdf6d08903fb620af1baef
Th0github <tdtsijpkens@gmail.com>
2020-05-13 20:53:01 +0200

    Added the all PoissonArrivals in Simulate.java.

--- src/Simulate.java
+++ src/Simulate.java
@@ -1,9 +1,6 @@
 import java.util.ArrayList;
 import java.util.Random;
 
-/**
- * @author ohnoyes
- */
 public class Simulate {
     public static void main(String[] args) {
         //Uniform Distribution
@@ -12,13 +9,16 @@ public class Simulate {
         NormalNumberGenerator rng = new NormalNumberGenerator();
         Corporate corporate = new Corporate();
         Consumer consumer = new Consumer();
+        int sampleCount = 100;
+        //Three shifts, for now we just record 10 events, either increase row count for more data or use Arraylist
+        double[][] timeShift = new double[3][sampleCount];
 
         //Arrival rates from calls: Poisson Process
         //Using Normal variate causes issues.
         PoissonProcess consumerArrival = new PoissonProcess(consumer.AVERAGE_RATE_PER_MINUTE, uniformVariate);
-        PoissonProcess corporateArrival = new PoissonProcess(corporate.RATE_EIGHT_am_to_SIX_pm, uniformVariate);
-        for(int i = 0; i <= 1000; i++) {
-            System.out.println(consumerArrival.events());
-        }
+        PoissonProcess consumerArrivalThreeAM = new PoissonProcess(consumer.AVERAGE_RATE_PER_MINUTE_3_AM, uniformVariate);
+        PoissonProcess corporateArrivalEightToSix = new PoissonProcess(corporate.RATE_EIGHT_am_to_SIX_pm, uniformVariate);
+        PoissonProcess corporateArrivalSixToEight = new PoissonProcess(corporate.RATE_SIX_am_to_EIGHT_am, uniformVariate);
+
     }
 }
